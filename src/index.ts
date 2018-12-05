44c83f8e7ce52749df7aaf5144942f0e5d411e6a
Stian Haga <hagastian@gmail.com>
2018-12-05 12:38:14 +0100

    refactor: fix uppercase

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const port = process.env.PORT || 3030
 const logger = new Logger(console.log)
 
 app.listen(port, () => {
-  logger.dump(chalk.green(`listening on port ${port}`))
+  logger.dump(chalk.green(`Listening on port ${port}`))
 })
